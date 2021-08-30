73004e3fdeddd7052f2b7e55135c06e550c3fca5
Ruslan Arlamovskiy <ruslanarlamovskiy@Ruslans-MacBook-Air.local>
2021-08-30 13:25:45 +0300

    .

--- patients/patients.routes.config.ts
+++ patients/patients.routes.config.ts
@@ -19,28 +19,40 @@ export class PatientsRoutes extends CommonRoutesConfig {
                 const firstNameReq = req.body.firstName;
                 const sexReq = req.body.sex;
                 const roleReq = req.body.role;
+                const children = req.body.children;
 
                 let oldRole = JSON.stringify(roleReq).replace(/@oneOf/g, '')
                                                      .replace('(', '')
                                                      .replace(')', '')
                                                      .replace(/"/g, '');
-                let RoleSplit = oldRole.split(",",2);
+                let oldChildren = JSON.stringify(children).replace(/@range/g, '')
+                                                          .replace('(', '')
+                                                          .replace(')', '')
+                                                          .replace(/"/g, '');
+                let roleSplit = oldRole.split(",",2);
+                let childrenSplit = oldChildren.split(",",2)
+
 
                 if (firstNameReq == '@firstName' && sexReq == '@sex' && roleReq != roleReq.replace(/@oneOf.*/)) {
                     let firstNameRes = faker.name.firstName()
                     let sexRes = faker.random.arrayElement(['Male', 'Female']);
-                    let roleRes = faker.random.arrayElement(RoleSplit)
+                    let roleRes = faker.random.arrayElement(roleSplit)
+                    let childrenRes = faker.datatype.number({
+                        'min': 0,
+                        'max': 4
+                    });
 
-                    console.log(firstNameRes + '\n' + sexRes + '\n' + roleRes)
+                    console.log(firstNameRes + '\n' +
+                                sexRes + '\n' +
+                                roleRes + '\n' +
+                                childrenRes)
                     res.status(200).send('First Name:\t' + firstNameRes + '\n' +
                                                      'Sex:\t' + sexRes + '\n' +
-                                                     'role:\t' + roleRes);
+                                                     'Role:\t' + roleRes + '' + '\n' +
+                                                     'Children:\t' + childrenRes);
 
                 }
-
             });
-
         return this.app;
     }
-
 }
