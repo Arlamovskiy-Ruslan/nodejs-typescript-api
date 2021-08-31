403bafa842b7014561cf59e0a059af13947688d3
Ruslan Arlamovskiy <ruslanarlamovskiy@Ruslans-MacBook-Air.local>
2021-08-31 15:10:24 +0300

    scratch

--- scratch.js
+++ scratch.js
@@ -1,51 +1,63 @@
+
 var o =
     {
         user: {
-            firstName: 'Adam',
-            sex: 'male',
-            role: 'user',
+            firstName: '@Adam',
+            sex: '@male',
+            role: '@user',
             children:
                 [
                     {
-                        name: 'Villiam',
-                        age: 6
+                        name: '@Villiam',
+                        age: "@"
                     }, {
-                    name: 'Danny',
-                    age: 8
+                    name: '@Danny',
+                    age: "@"
                 }
                 ]
         }
     };
-// function traverse(o, func) {
-//     const stack = [o];
-//
-//     while (stack.length) {
-//         const obj = stack.shift()
-//
-//         Object.keys(obj).forEach((key) => {
-//             func(key, obj[key], obj)
-//             if (obj[key] instanceof Object) {
-//                 stack.unshift(obj[key])
-//                 return
-//             }
-//         })
-//     }
-// }
-//
-// const fx = (key, value, obj) => console.log(key, value)
-// traverse(o, fx)
-
-function process(key, value) {
-    console.log(key + " : " + value);
-}
 
 function traverse(node, func) {
-    for (var prop in node) {
-        func(prop, node[prop]);
-        if (node[prop] !== null && typeof (node[prop]) == "object") {
-            traverse(node[prop], func);
-        }
+    const stack = [node];
+
+    while (stack.length) {
+        const node = stack.shift()
+
+        Object.keys(node).forEach((key) => {
+            func(key, node[key], node)
+            if (node[key] instanceof Object) {
+
+                stack.unshift(node[key])
+                return
+            }
+        })
     }
 }
 
-traverse(o, process);
+// const fx = (key, value) => console.log(key, value)
+const fx = (key, value) => {
+    if (value === "@Adam"){
+        console.log("SSSS")
+    }
+    if (value === "@male"){
+        console.log("SS")
+    }
+}
+
+traverse(o, fx)
+
+// function process(key, value) {
+//     console.log(key + " : " + value);
+// }
+//
+// function traverse(node, func) {
+//     for (var prop in node) {
+//         func(prop, node[prop]);
+//         if (node[prop] !== null && typeof (node[prop]) == "object") {
+//             traverse(node[prop], func);
+//         }
+//     }
+// }
+
+// traverse(o, process);
