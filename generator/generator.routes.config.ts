a3fdfe9ed6aa59b55cf8108fd6c56aa744dbf664
Ruslan Arlamovskiy <ruslanarlamovskiy@Ruslans-MacBook-Air.local>
2021-09-01 22:14:56 +0300

    generator

--- generator/generator.routes.config.ts
+++ generator/generator.routes.config.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import {traverse} from "../bypassingTheTree/bypassTree";
-import {translator} from "../generalist/generalist";
+import {translate} from "../generalist/generalist";
+import {transform} from "../translate/transform/transform";
 
 export class PatientsRoutes {
 
@@ -8,7 +9,7 @@ export class PatientsRoutes {
 
         app.route(`/generate`)
             .post((req: express.Request, res: express.Response) => {
-                res.status(200).send(traverse(req.body, translator))
+                res.status(200).send(traverse(req.body, transform))
             });
         return app;
         
