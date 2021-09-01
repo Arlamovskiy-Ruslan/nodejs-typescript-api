1cd539d3a16ec743dcd0c2a850dc3387b256ff0b
Ruslan Arlamovskiy <ruslanarlamovskiy@Ruslans-MacBook-Air.local>
2021-09-01 14:08:38 +0300

    bypass tree

--- bypassingTheTree/bypassTree.ts
+++ bypassingTheTree/bypassTree.ts
@@ -1,12 +1,12 @@
-export function traverse(node: any, func: any) {
+export function traverse(node: any, func: (key: string, value: string, node: object) => void) {
     const stack = [node];
 
     while (stack.length) {
         const node = stack.shift()
 
         Object.keys(node).forEach((key) => {
-            func(key, node[key], node)
             if (node[key] instanceof Object) {
+                func(key, node[key], node)
                 stack.unshift(node[key])
                 return
             }
