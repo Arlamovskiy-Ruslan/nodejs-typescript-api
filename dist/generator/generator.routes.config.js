f7b369bda42ef99a6b96f20a7d9ff7b916183ddf
Ruslan Arlamovskiy <ruslanarlamovskiy@Ruslans-MacBook-Air.local>
2021-09-01 14:09:06 +0300

    compile js

--- dist/generator/generator.routes.config.js
+++ dist/generator/generator.routes.config.js
@@ -1,21 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PatientsRoutes = void 0;
-const common_routes_config_1 = require("../common/common.routes.config");
 const bypassTree_1 = require("../bypassingTheTree/bypassTree");
 const generalist_1 = require("../generalist/generalist");
-class PatientsRoutes extends common_routes_config_1.CommonRoutesConfig {
-    constructor(app) {
-        super(app, 'PatientsRoutes');
-    }
-    configureRoutes() {
-        this.app.route(`/generator`)
+class PatientsRoutes {
+    configureRoutes(app) {
+        app.route(`/generator`)
             .post((req, res) => {
-            const fx = (key, value) => console.log(generalist_1.randomFirstName(key, value));
-            res.status(200).send(bypassTree_1.traverse(req.body, fx));
+            res.status(200).send(bypassTree_1.traverse(req.body, generalist_1.translator));
         });
-        return this.app;
+        return app;
     }
 }
 exports.PatientsRoutes = PatientsRoutes;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2VuZXJhdG9yLnJvdXRlcy5jb25maWcuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9nZW5lcmF0b3IvZ2VuZXJhdG9yLnJvdXRlcy5jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQUEseUVBQWtFO0FBRWxFLCtEQUF3RDtBQUN4RCx5REFBeUQ7QUFFekQsTUFBYSxjQUFlLFNBQVEseUNBQWtCO0lBQ2xELFlBQVksR0FBd0I7UUFDaEMsS0FBSyxDQUFDLEdBQUcsRUFBRSxnQkFBZ0IsQ0FBQyxDQUFDO0lBQ2pDLENBQUM7SUFFRCxlQUFlO1FBRVgsSUFBSSxDQUFDLEdBQUcsQ0FBQyxLQUFLLENBQUMsWUFBWSxDQUFDO2FBQ3ZCLElBQUksQ0FBQyxDQUFDLEdBQW9CLEVBQUUsR0FBcUIsRUFBRSxFQUFFO1lBQ2xELE1BQU0sRUFBRSxHQUFHLENBQUMsR0FBTyxFQUFFLEtBQVMsRUFBRSxFQUFFLENBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQyw0QkFBZSxDQUFDLEdBQUcsRUFBRSxLQUFLLENBQUMsQ0FBQyxDQUFBO1lBQzNFLEdBQUcsQ0FBQyxNQUFNLENBQUMsR0FBRyxDQUFDLENBQUMsSUFBSSxDQUFDLHFCQUFRLENBQUMsR0FBRyxDQUFDLElBQUksRUFBRSxFQUFFLENBQUMsQ0FBQyxDQUFBO1FBQ2hELENBQUMsQ0FBQyxDQUFDO1FBQ1AsT0FBTyxJQUFJLENBQUMsR0FBRyxDQUFDO0lBRXBCLENBQUM7Q0FDSjtBQWZELHdDQWVDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ2VuZXJhdG9yLnJvdXRlcy5jb25maWcuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9nZW5lcmF0b3IvZ2VuZXJhdG9yLnJvdXRlcy5jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQ0EsK0RBQXdEO0FBQ3hELHlEQUFvRDtBQUVwRCxNQUFhLGNBQWM7SUFFdkIsZUFBZSxDQUFDLEdBQXdCO1FBRXBDLEdBQUcsQ0FBQyxLQUFLLENBQUMsWUFBWSxDQUFDO2FBQ2xCLElBQUksQ0FBQyxDQUFDLEdBQW9CLEVBQUUsR0FBcUIsRUFBRSxFQUFFO1lBQ2xELEdBQUcsQ0FBQyxNQUFNLENBQUMsR0FBRyxDQUFDLENBQUMsSUFBSSxDQUFDLHFCQUFRLENBQUMsR0FBRyxDQUFDLElBQUksRUFBRSx1QkFBVSxDQUFDLENBQUMsQ0FBQTtRQUN4RCxDQUFDLENBQUMsQ0FBQztRQUNQLE9BQU8sR0FBRyxDQUFDO0lBRWYsQ0FBQztDQUNKO0FBWEQsd0NBV0MifQ==
