97a68028aa0fe11811b2c85479e888ea23117f78
Ruslan Arlamovskiy <ruslanarlamovskiy@Ruslans-MacBook-Air.local>
2021-09-02 13:38:35 +0300

    transform

--- translate/transform/transform.ts
+++ translate/transform/transform.ts
@@ -2,13 +2,13 @@ import {ITranslate} from "../interface/translate";
 import {randomName} from "../randomName";
 import {randomSex} from "../randomSex";
 import {oneOf} from "../oneOf";
-import {rangeChildren} from "../rangeChildren";
+import {rangeAge} from "../rangeAge";
 
 export function transform(key: string, value: string){
     let randomFirstNameV: ITranslate = new randomName()
     let randomSexV: ITranslate = new randomSex()
     let oneOfSex: ITranslate = new oneOf()
-    let rangeChildrenV: ITranslate = new rangeChildren()
+    let rangeChildrenV: ITranslate = new rangeAge()
 
     randomFirstNameV.translate?.(key, value)
     randomSexV.translate?.(key, value)
