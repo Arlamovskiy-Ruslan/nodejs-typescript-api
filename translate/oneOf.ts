ee23c17ed56565795be976347ecf3fab2b3d72c6
Ruslan Arlamovskiy <ruslanarlamovskiy@Ruslans-MacBook-Air.local>
2021-09-02 13:11:51 +0300

    oneOf

--- translate/oneOf.ts
+++ translate/oneOf.ts
@@ -5,8 +5,14 @@ export class oneOf implements ITranslate {
     translate(key: string, value: string): void {
         let role = JSON.stringify(value).toLowerCase().match(/@o.*/);
         if (role) {
-
-            value = faker.random.arrayElement(['adm', 'usr'])
+            let oldRole = JSON.stringify(role).toLowerCase().replace(/@oneOf/g, ' ')
+                .replace(/@oneof/g, ' ').replace('[', '')
+                .replace(/"/g, '').replace('(', '')
+                .replace(')', '').replace(/\\/g, '')
+                .replace('/', '').replace('{', '')
+                .replace('}', '').replace(']', '');
+            let splitRole = oldRole.split(",", 2)
+            value = faker.random.arrayElement(splitRole)
             console.log(value)
         }
     };
