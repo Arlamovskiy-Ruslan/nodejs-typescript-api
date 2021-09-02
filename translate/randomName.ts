70fb2ceecf1542a84f4edfdb56d93424ed56e14d
Ruslan Arlamovskiy <ruslanarlamovskiy@Ruslans-MacBook-Air.local>
2021-09-02 13:12:42 +0300

    translate @firstName

--- translate/randomName.ts
+++ translate/randomName.ts
@@ -1,7 +1,7 @@
 import faker from "faker";
 import {ITranslate} from "./interface/translate";
 
-export class randomName implements ITranslate{
+export class randomName implements ITranslate {
     translate(key: string, value: string): void {
         let firstName = JSON.stringify(value).toLowerCase().match(/@f.*/);
         if (firstName) {
